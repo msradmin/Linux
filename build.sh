f4977ec35b9ec8705bf325a2ad97a4e769477a08
msroffice <86022985+msradmin@users.noreply.github.com>
2023-03-23 08:54:38 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo | Downloading Windows 7 ( Portuguese Brazilian ) |
+wget https://archive.org/download/Windows7_x86-x64_ptBR_Pack_MSDN/pt_windows_7_ultimate_with_sp1_x64_dvd_u_677358.iso
+echo | Download concluído. |
+echo - Agora você precisa de um Pendrive para completar a instalação.
+echo - dê um (./build1.sh) para terminar a instalação.
+echo ( Código básico feito por Rafael Rodrigues ( aka MSR ) )
